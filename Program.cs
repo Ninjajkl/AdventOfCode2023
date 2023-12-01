3f175efcf0ebdb0715ca415ab755cbcb4d0dd528
Ninjajkl <ninjajkl@gmail.com>
2023-12-01 12:30:28 -0500

    init: added initial project and day manager

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdventOfCode2023 // Note: actual namespace depends on the project name.
+{
+    class DayManager
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
